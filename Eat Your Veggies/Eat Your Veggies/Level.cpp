1ff0964ddd336a8502b05061a868d30fcf8aa4b3
JackNulty <93530525+JackNulty@users.noreply.github.com>
2023-07-07 22:41:53 +0100

    added moving level

--- Eat Your Veggies/Eat Your Veggies/Level.cpp
+++ Eat Your Veggies/Eat Your Veggies/Level.cpp
@@ -2,13 +2,17 @@
 
 void Level::init()
 {
-	if (!floorTexture.loadFromFile("ASSETS/SPRITES/Faceset.png"))
+	if (!floorTexture.loadFromFile("ASSETS/SPRITES/Counter.png"))
 	{
 		std::cout << "error" << std::endl;
 	}
 	floorSprite.setTexture(floorTexture);
 	floorSprite.setPosition(floorPosX, floorPOsY);
 	floorSprite.setScale(1, 1);
+
+	floorSprite1.setTexture(floorTexture);
+	floorSprite1.setPosition(floorPosX1, floorPOsY1);
+	floorSprite1.setScale(1, 1);
 }
 
 void Level::update()
@@ -17,11 +21,20 @@ void Level::update()
 	floorSprite.setPosition(floorPosX, floorPOsY);
 	if (floorPosX >= SCREEN_WIDTH)
 	{
-		floorPosX = 0;
+		floorPosX = -1200;
+	}
+	std::cout << "1st platform pos" << floorPosX << std::endl;
+	std::cout << "2nd platform pos" << floorPosX1 << std::endl;
+	floorPosX1++;
+	floorSprite1.setPosition(floorPosX1, floorPOsY1);
+	if (floorPosX1 >= SCREEN_WIDTH)
+	{
+		floorPosX1 = -1200;
 	}
 }
 
 void Level::render(sf::RenderWindow& window)
 {
 	window.draw(floorSprite);
+	window.draw(floorSprite1);
 }
--- Eat Your Veggies/Eat Your Veggies/Level.h
+++ Eat Your Veggies/Eat Your Veggies/Level.h
@@ -10,9 +10,14 @@ public:
 	sf::Sprite floorSprite;
 	sf::Texture floorTexture;
 
+	sf::Sprite floorSprite1;
+
 private:
 
 	int floorPosX = 0;
 	int floorPOsY = 650;
+
+	int floorPosX1 = -1200;
+	int floorPOsY1 = 650;
 
 };
