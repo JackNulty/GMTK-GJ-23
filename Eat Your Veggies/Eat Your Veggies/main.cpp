f8588eab71d4da5058b1024cc477738119e08dba
ShaneConroy <93271006+ShaneConroy@users.noreply.github.com>
2023-07-07 19:52:56 +0100

    Added beginner code
    Description

--- Eat Your Veggies/Eat Your Veggies/main.cpp
+++ Eat Your Veggies/Eat Your Veggies/main.cpp
@@ -1,1 +1,43 @@
-//porn
+// Project: Eet ouy Veggies
+
+#ifdef _DEBUG  
+#pragma comment(lib,"sfml-graphics-d.lib")  
+#pragma comment(lib,"sfml-audio-d.lib")  
+#pragma comment(lib,"sfml-system-d.lib")  
+#pragma comment(lib,"sfml-window-d.lib")  
+#pragma comment(lib,"sfml-network-d.lib")  
+#else  
+#pragma comment(lib,"sfml-graphics.lib")  
+#pragma comment(lib,"sfml-audio.lib")  
+#pragma comment(lib,"sfml-system.lib")  
+#pragma comment(lib,"sfml-window.lib")  
+#pragma comment(lib,"sfml-network.lib")  
+#endif 
+
+#include <SFML/Graphics.hpp> 
+#include <stdlib.h>  
+#include <iostream>
+
+int main()
+{
+	const int SCREEN_WIDTH = 1400;
+	const int SCREEN_HEIGHT = 800;
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Eat your Beggies");
+
+	// Update
+	while (window.isOpen())
+	{
+		// Event loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// Draw
+		window.clear();
+		window.display();
+	}
+
+}
